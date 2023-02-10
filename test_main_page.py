1215a433872e6f9759620d2f3c8d919780b03466
MariaByw <97654459+MariaByw@users.noreply.github.com>
2023-02-10 21:08:02 +0500

    Первоначальная загрузка

---
+++ test_main_page.py
@@ -0,0 +1,22 @@
+from selenium.common.exceptions import NoSuchElementException
+import time
+from selenium.webdriver.common.by import By
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def basket_should_be_present(browser):
+    try:
+        browser.find_element(By.CSS_SELECTOR,"button.btn-add-to-basket")
+        return True
+    except NoSuchElementException:
+        return False
+
+def test_basket_shuld_be_visible(browser):
+    browser.get(link)
+    time.sleep(5)
+    assert basket_should_be_present(browser), "Basket element shuld be present."
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
