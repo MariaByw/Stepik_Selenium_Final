37364b547b8ae693441ee49d1094f65ef2cdea9d
MariaByw <97654459+MariaByw@users.noreply.github.com>
2023-02-11 16:21:53 +0500

    Added test_guest_should_see_login_link. Modified BasePage class

--- test_main_page.py
+++ test_main_page.py
@@ -1,24 +1,30 @@
-from selenium.common.exceptions import NoSuchElementException
-import time
-from selenium.webdriver.common.by import By
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+from .pages.main_page import MainPage
+# link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def basket_should_be_present(browser):
-    try:
-        browser.find_element(By.CSS_SELECTOR,"button.btn-add-to-basket")
-        return True
-    except NoSuchElementException:
-        return False
-
-def test_basket_shuld_be_visible(browser):
-    browser.get(link)
-    time.sleep(5)
-    assert basket_should_be_present(browser), "Basket element shuld be present."
-
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+# def basket_should_be_present(browser):
+#     try:
+#         browser.find_element(By.CSS_SELECTOR,"button.btn-add-to-basket")
+#         return True
+#     except NoSuchElementException:
+#         return False
+#
+# def test_basket_shuld_be_visible(browser):
+#     browser.get(link)
+#     time.sleep(5)
+#     assert basket_should_be_present(browser), "Basket element shuld be present."
+#
+# def go_to_login_page(browser):
+#     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+#     login_link.click()
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
